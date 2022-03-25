c098c76cf2198f4ab348fc08c33ccdda5c500762
sangheekim <tkdgmltjddnr@naver.com>
2022-03-25 17:52:37 +0900

    es6 변환

--- js/scroll_es6.js
+++ js/scroll_es6.js
@@ -2,44 +2,33 @@ class Scroll{
     constructor(){
         this.init();
         this.bindingEvent();
-        this.resize();
+        this.setPos();
     }
     init(){
-        this.sections  = document.querySelectorAll('section');
+        this.sections= document.querySelectorAll('section');
         this.ul = document.querySelector('ul');
-        this.btns = this.ul.querySelectorAll('ul li');
-        this.btnsArr = Array.from(this.btns);
-        this.arrSec = Array.from(this.sections);
+        this.lis = document.querySelectorAll('ul li'); 
+        this.lis_arr = Array.from( this.lis);
         this.posArr = null;
         this.enableClick = true;
     }
     bindingEvent(){
-        this.btns.forEach((el,idx)=>{
-            el.addEventListener('click', e=>{
+        window.addEventListener('resize',this.setPos);
+        window.addEventListener('scroll',this.activation);
+        this.lis.forEach((li,idx)=>{
+            li.addEventListener('click',e=>{
                 if(this.enableClick){
                     this.enableClick=false;
                     e.preventDefault();
-                    this.scrollAni(idx);
+                    this.movescroll(idx);
                 }
             });
         });
-        window.addEventListener('scroll',this.scrollActive);
-        window.addEventListener('resize', this.resize);
         this.sections.forEach(section=>{
             section.addEventListener('mousewheel',e=>{
                 e.preventDefault();
                 this.mousewheel(e);
             })
-        });
-    }
-    scrollAni(idx){
-        new Animate(window,{
-            prop : 'scroll',
-            value : this.posArr[idx],
-            duration : 500,
-            callback : ()=> {
-                this.enableClick =true;
-            }
         });
     }
     mousewheel(e){
@@ -48,43 +37,55 @@ class Scroll{
             const activeEl = document.querySelector('section.on');
             const arrSec = Array.from(this.sections);
             const activeIdx = arrSec.indexOf(activeEl);
+    
+            
             if(e.deltaY > 0){
                 if(activeIdx!== this.sections.length-1){
-                    this.scrollAni(activeIdx + 1);
+                    this.movescroll(activeIdx + 1);
                 }else{
                     this.enableClick = true;
                 }
             }
             if (e.deltaY < 0){
                 if(activeIdx !== 0){
-                    this.scrollAni(activeIdx-1);
+                    this.movescroll(activeIdx-1);
                 } else{
                     this.enableClick = true;
                 }
             }
-        }   
-    } 
-    scrollActive(){
-        const scroll = window.scrollY;
-        this.sections.forEach(sec,idx=>{
+            
+        }    
+    }
+    setPos(){
+        this.posArr = [];
+        for(const sec of this.sections) this.posArr.push(sec.offsetTop);
+        const active = this.ul.querySelector('li.on');
+        const activeIndex = this.lis_arr.indexOf(active);  
+        window.scroll(0,this.posArr[activeIndex]); 
+    }
+    activation(){
+        let scroll = window.scrollY || window.pageYOffset;
+        this.sections.forEach((sec,idx)=>{
             if(scroll >= this.posArr[idx]){
-                for(const btn of this.btns){
-                    btn.classList.remove('on');
-                    this.btns[idx].classList.add('on');
+                for(const li of this.lis){
+                    li.classList.remove('on');
+                    this.lis[idx].classList.add('on');
                 }
                 for(const section of this.sections){
                     section.classList.remove('on');
                     sec.classList.add('on');
-                }   
+                }
             }
         });
-    } 
-    resize(){ 
-        //각 섹션의 offsetTop값 구해서 빈배열에 담기
-        this.posArr = [];
-        for(const sec of this.sections) this.posArr.push(sec.offsetTop);
-        const active = this.ul.querySelector('li.on');
-        const activeIdx = this.btnsArr.indexOf(active);
-        window.scroll(0,this.posArr[activeIdx]); 
+    }
+    movescroll(idx){
+        new Animate(window,{
+            prop : 'scroll',
+            value : this.posArr[idx],
+            duration : 500,
+            callback :()=>{
+                this.enableClick =true;
+            }
+        });
     }
 }
